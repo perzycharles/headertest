fa921eb28d7945e63992768fb9a3c9e8dca3ed0a
yong zuo <yongchongqing@gmail.com>
2019-03-20 12:06:58 +0900

    first

---
+++ web/index.php
@@ -0,0 +1,17 @@
+<?php
+?>
+
+<html>
+<head>
+ <a href="test.php"></a>
+ <script type="text/javascript" src="test.js">
+ <link rel="stylesheet" type="text/css" href="test.css">
+</head>
+<body>
+
+<h1>I am formatted with a linked style sheet</h1>
+<p id ='cn'>Me too!</p>
+<button type="button" onclick="changefont()">Click</button>
+
+</body>
+</html>
---
+++ web/test.php
@@ -0,0 +1,12 @@
+<?php
+?>
+<html>
+<head>
+</head>
+<body>
+
+<h1>Hello</h1>
+<p>Yes</p>
+
+</body>
+</html>
