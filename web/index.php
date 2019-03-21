73a4ccca42a7822fc5dd2c725b2abe7dc1f674af
perzycharles <37419121+perzycharles@users.noreply.github.com>
2019-03-22 08:50:14 +0900

    add html test

--- web/index.php
+++ web/index.php
@@ -3,7 +3,6 @@
 
 <html>
 <head>
- <a href="test.php"></a>
  <script type="text/javascript" src="test.js"></script>
  <link rel="stylesheet" type="text/css" href="test.css">
 </head>
@@ -12,6 +11,7 @@
 <h1>I am formatted with a linked style sheet</h1>
 <p id ='cn'>Me too!</p>
 <button type="button" onclick="changefont()">Click</button>
-
+ <a href="test.php">php test</a>
+ <a href="test.html">html test</a>
 </body>
 </html>
